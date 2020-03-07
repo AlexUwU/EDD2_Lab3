d74b80ad7a0ba3a8d8f6136672ef2d65e046dc1a
AlexUwU <alexander.2014022@gmail.com>
2020-03-06 19:09:17 -0600

    clase serializer

---
+++ HuffmanCompresion/HuffmanCompresion/Controllers/Serializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace HuffmanCompresion.Controllers
+{
+    internal class Serializer
+    {
+        internal static void GuardarArchivoBinario<T>(string filePath, T pObject)
+        {
+            try
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    var formatBin = new BinaryFormatter();
+
+                    formatBin.Serialize(memoryStream, pObject);
+
+                    string path = Path.GetDirectoryName(filePath);
+
+                    if (!Directory.Exists(path))
+                        Directory.CreateDirectory(path);
+
+                    File.WriteAllBytes(filePath, memoryStream.ToArray());
+
+                    memoryStream.Close();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
