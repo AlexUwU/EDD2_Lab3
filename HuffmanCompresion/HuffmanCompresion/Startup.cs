9215b628ea1e653d23755ee1a681f5bbca329209
Rodrigo <Villacinda>
2020-03-07 19:11:05 -0600

    lector de archivos

--- HuffmanCompresion/HuffmanCompresion/Startup.cs
+++ HuffmanCompresion/HuffmanCompresion/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -35,6 +36,10 @@ namespace HuffmanCompresion
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
 
@@ -42,9 +47,14 @@ namespace HuffmanCompresion
 
             app.UseAuthorization();
 
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+            app.Run(async (context) => {
+                await context.Response.WriteAsync("Could not Find Anything");
             });
         }
     }
